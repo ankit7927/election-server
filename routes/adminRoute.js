a1309a7b7666bc6cec5a1b403b617b9e1c5f5612
ankit <code7lancer@gmail.com>
2022-09-02 09:02:23 -0700

    updated more easy way

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const adminAuth = require("../authentication/adminAuth");
 const {
   createNewElection,
+  deteletElection,
   nominateCandidate,
   getAllVoters,
   registerVoter,
@@ -23,27 +24,32 @@ router.post("/new-election", imageHandle.single("eleImage"), (req, res) => {
   createNewElection(req, res);
 });
 
+router.delete("/dele-election/:eleID", (req, res) => {
+  deteletElection(req, res);
+});
+
 router.get("/all-election", (req, res) => {
   getAllElection(req, res);
 });
 
-router.post("/new-cand", adminAuth, (req, res) => {
+router.post("/new-cand", (req, res) => {
   nominateCandidate(req, res);
 });
 
-router.get("/all-cand/:electionID", adminAuth, (req, res) => {
+router.get("/all-cand/:electionID", (req, res) => {
   getAllCandidateOfElection(req, res);
 });
 
-router.get("/all-voter", adminAuth, (req, res) => {
+// voters
+router.get("/all-voter", (req, res) => {
   getAllVoters(req, res);
 });
 
-router.get("/reg-voter/:eleID", adminAuth, (req, res) => {
+router.get("/reg-voter/:eleID", (req, res) => {
   registerVoter(req, res);
 });
 
-router.get("/current-ele", adminAuth, (req, res) => {
+router.get("/current-ele", (req, res) => {
   getOnGoingElection(req, res);
 });
 
