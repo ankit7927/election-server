1dc07fdd7f30a83ef7f5838b4030ee2b3fbb30aa
ankit <code7lancer@gmail.com>
2022-09-14 06:34:42 -0700

    switched to mongoose@5 and working stable...

--- database/conn.js
+++ database/conn.js
@@ -4,9 +4,10 @@ require("dotenv").config()
 const remoteDB = `mongodb+srv://Ankit:${process.env.DBPASS}@electiondb.nxv1f0v.mongodb.net/?retryWrites=true&w=majority`
 const localDB = "mongodb://localhost:27017/electionDB";
 
-mongoose.connect(remoteDB, {
+mongoose.connect(localDB, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: trues
 }).then(() => {
   console.log("connected to database ✔️")
 }).catch((err) => console.log("can't connect to database ❌" + "\n" + err))
