12f8d6acc0a65723ac273fc0ea2afd4331ecb292
ankit <code7lancer@gmail.com>
2023-01-11 19:01:19 +0530

    v2.2.0-done

--- database/conn.js
+++ database/conn.js
@@ -3,8 +3,9 @@ require("dotenv").config()
 
 const remoteDB = `mongodb+srv://Ankit:${process.env.DBPASS}@electiondb.nxv1f0v.mongodb.net/?retryWrites=true&w=majority`
 const localDB = "mongodb://localhost:27017/electionDB";
+const testDBUrl = require("dbUrl.txt")
 
-mongoose.connect(localDB, {
+mongoose.connect(testDBUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true     //for mongoose v5.*,,, remove in using latest version of mongoose
